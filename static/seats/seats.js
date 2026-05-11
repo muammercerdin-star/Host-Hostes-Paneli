6b7b2f9cf067edefc79f1bbef8427ca33ebebd2a
muammercerdin-star <muammercerdin@gmail.com>
2026-05-12 01:26:17 +0300

    koltuk sayaci duzeltildi

--- static/seats/seats.js
+++ static/seats/seats.js
@@ -725,6 +725,10 @@ function setVoiceBadge(text){
 
     setText("#pillTotal", String(filled + standingCount));
     setText("#pillEmpty", String(empty));
+    setText("#voiceSeatFilled", String(filled));
+    setText("#voiceSeatEmpty", String(empty));
+    setText("#driveVoiceFilled", String(filled));
+    setText("#driveVoiceEmpty", String(empty));
     setText("#pillStanding", String(standingCount));
     setText("#pillCash", fmtTL(standingRevenue));
     setText("#pillService", String(totalServiceCount()));
